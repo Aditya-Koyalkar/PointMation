8a019f3722f9bbfb5823b0169427f95e93cbce0b
KoyalkarAditya <koyalkaraditya123@gmail.com>
2025-05-06 22:27:06 +0530

    added x theme and dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import VideoPlayer from "./_components/VideoPlayer";
+
 export default async function Home() {
-  return <div className="p-4"></div>;
+  return (
+    <div className="p-4">
+      <VideoPlayer />
+    </div>
+  );
 }
