855d50626b54e6398e6037286c972d11293f0089
KoyalkarAditya <koyalkaraditya123@gmail.com>
2025-05-11 12:55:45 +0530

    fixes

--- apps/code-worker/src/index.ts
+++ apps/code-worker/src/index.ts
@@ -20,7 +20,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_DOMAIN,
     credentials: true,
     methods: ["PUT", "POST", "GET", "DELETE"],
   },
